dccf41c6a1d79724a01f3a38adb1a752418e5e2a
CharlesPikachu <1159254961@qq.com>
2022-07-20 23:06:45 +0800

    update docs

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     license=paperdl.__license__,
     include_package_data=True,
     entry_points={'console_scripts': ['paperdl = paperdl.paperdl:paperdlcmd']},
-    install_requires=list(open('requirements.txt', 'r').readlines()),
+    install_requires=[lab.strip('\n') for lab in list(open('requirements.txt', 'r').readlines())],
     zip_safe=True,
     packages=find_packages()
 )
